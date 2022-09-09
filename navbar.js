33e2e758f0bdf5745be3e00d4033618f8d393d52
Abel Gonzalez <fakekiwis@gmail.com>
2022-09-09 08:48:50 -0500

    Web Analytics

--- navbar.js
+++ navbar.js
@@ -15,3 +15,18 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+//<![CDATA[
+var owa_baseUrl = 'http://your.domain.com/path/to/owa/';
+var owa_cmds = owa_cmds || [];
+owa_cmds.push(['setSiteId', 'your_site_id']);
+owa_cmds.push(['trackPageView']);
+owa_cmds.push(['trackClicks']);
+owa_cmds.push(['trackDomStream']);
+
+(function() {
+	var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+	_owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+	var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+}());
+//]]>
